7a23d7d0e9edbc9bc87ba79932127af12f520ee8
Adrián Janočko <adonikeoffice@gmail.com>
2025-03-16 11:18:33 +0100

    Renamed the Heading type to make it reusable

--- src/components/H1.tsx
+++ src/components/H1.tsx
@@ -1,7 +1,7 @@
-import { Heading } from "@/util/types";
+import { WrapperProps } from "@/util/types";
 import { twMerge } from "tailwind-merge";
 
-export default function H1({ children, className }: Heading) {
+export default function H1({ children, className }: WrapperProps) {
   return (
     <h1 className={twMerge("font-bold text-6xl", className)}>{children}</h1>
   );
