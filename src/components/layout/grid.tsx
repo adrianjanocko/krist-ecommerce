05195d7eeb067d38767ba11606b98e3eb1160d8c
Adrián Janočko <adonikeoffice@gmail.com>
2025-03-19 22:02:44 +0100

    Minor fix

--- src/components/layout/grid.tsx
+++ src/components/layout/grid.tsx
@@ -2,5 +2,5 @@ import { WrapperProps } from "@/util/types";
 import { twMerge } from "tailwind-merge";
 
 export default function Grid({ children, className }: WrapperProps) {
-  return <div className={twMerge("grid gap-6", className)}>{children}</div>;
+  return <div className={twMerge("grid gap-8", className)}>{children}</div>;
 }
